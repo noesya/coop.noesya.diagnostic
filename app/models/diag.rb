24b5dfba6aa728680cabc2315a1c696d77a54a15
Arnaud Levy <contact@arnaudlevy.com>
2021-11-10 18:57:16 +0100

    no pwa

--- app/models/diag.rb
+++ app/models/diag.rb
@@ -95,8 +95,9 @@ class Diag < ApplicationRecord
     command = "lighthouse #{self.url}"
     command += " --output json"
     command += " --output-path #{local_path}"
-    command += " --skip-audits=full-page-screenshot"
-    command += " --chrome-flags=\"--headless --ignore-certificate-errors\""
+    command += " --skip-audits=pwa"
+    # command += " --skip-audits=full-page-screenshot"
+    # command += " --chrome-flags=\"--headless --ignore-certificate-errors\""
     puts command
     system command
     data = File.read local_path
