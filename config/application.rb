7eb0db132759b8b6becfe0f34f875af6110dc814
Arnaud Levy <contact@arnaudlevy.com>
2021-11-10 13:52:29 +0100

    dj

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Diagnostic
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :delayed_job
 
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
