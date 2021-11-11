38eebac7685b5177d58fda68f6d86c5ccfa5604d
Arnaud Levy <contact@arnaudlevy.com>
2021-11-11 12:09:21 +0100

    capital constant

--- app/services/slack_notification.rb
+++ app/services/slack_notification.rb
@@ -1,7 +1,7 @@
 class SlackNotification
   def self.push(message)
-    return unless ENV['slack_hook']
-    slack = Slack::Incoming::Webhooks.new ENV['slack_hook']
+    return unless ENV['SLACK_HOOK']
+    slack = Slack::Incoming::Webhooks.new ENV['SLACK_HOOK']
     slack.post message
   end
 end
