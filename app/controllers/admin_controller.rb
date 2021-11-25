223ae9bed84bcce8942fa2d85a7bae98f293e14a
Arnaud Levy <contact@arnaudlevy.com>
2021-11-25 19:45:52 +0100

    per 50

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
   def index
-    @diags = Diag.all.order(created_at: :desc).page(params[:page]).per(5)
+    @diags = Diag.all.order(created_at: :desc).page(params[:page]).per(50)
   end
 end
