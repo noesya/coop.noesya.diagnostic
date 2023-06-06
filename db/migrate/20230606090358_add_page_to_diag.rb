ec95957c249b05b2011768af0bf8bda87add839d
Arnaud Levy <contact@arnaudlevy.com>
2023-06-06 12:54:57 +0200

    migration

--- db/migrate/20230606090358_add_page_to_diag.rb
+++ db/migrate/20230606090358_add_page_to_diag.rb
@@ -2,7 +2,8 @@ class AddPageToDiag < ActiveRecord::Migration[6.1]
   def change
     add_reference :diags, :page, foreign_key: true, type: :uuid
     Diag.all.select(:id, :url, :page_id).find_each do |diag|
-      page = Page.create url: diag.url
+      url = diag.attributes[:url]
+      page = Page.where(url: url).first_or_create
       diag.update_column :page_id, page.id
     end
     change_column :diags, :page_id, :uuid, null: false
