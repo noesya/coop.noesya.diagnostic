b1ea73d9ee91615c4aca99061223a4f6f54f8f8b
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-06-21 17:23:42 +0200

    batch is better

--- app/jobs/fix_screenshot_job.rb
+++ app/jobs/fix_screenshot_job.rb
@@ -2,7 +2,7 @@ class FixScreenshotJob < ApplicationJob
   queue_as :default
 
   def perform
-    Diag.where(screenshot: nil).where.not(lighthouse: nil).each do |diag|
+    Diag.where(screenshot: nil).where.not(lighthouse: nil).find_each(batch_size: 50) do |diag|
       diag.send(:get_screenshot)
     end
   end
